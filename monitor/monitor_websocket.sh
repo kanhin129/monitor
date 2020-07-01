e9c7d2b084c3a1ef19b9a5486aa26f015ea4af5f
James <james@bcowtech.com>
2020-07-01 14:59:09 +0800

    monitor_websocket.sh 修改第三次掃描改用Array存放Error Domain

--- monitor/monitor_websocket.sh
+++ monitor/monitor_websocket.sh
@@ -4,12 +4,11 @@ BOT='/root/gitlab-project/manager-tools/python_bot/zbxtg_group.py'
 GROUP='BCowtech-alert'
 #GROUP='james-test'
 HOST=`hostname`
-tmp_file='/tmp/tmp_ws.txt'
 array=()
+array2=()
 
 #wscat -c wss://ws.silverlighting.net/fishingking/lobby -x q
 
-#for line in $(cat $LIST);do
 #第一次檢測 level1
 while read line; do
     res=`python3 /root/gitlab-project/manager-tools/monitor/conn_ws.py $line 2>&1 | head -n 1 `
@@ -23,27 +22,24 @@ sleep 10
 
 #第二次檢測 level2
 for ((i=0; i<${#array[@]}; i++)); do
-    #echo ${array[$i]}
     ws_domain=${array[$i]}
     res=`python3 /root/gitlab-project/manager-tools/monitor/conn_ws.py $ws_domain 2>&1 | head -n 1 `
     if [ "$res" != "ok" ]; then
         python $BOT "$GROUP" "From-${HOST}" "$(echo -e "Problem: WS Error {{fire}}{{fire}}Level2{{fire}}{{fire}}  \nDomain: ${ws_domain}\nStatus: ${res}")"
         echo "$ws_domain" >> $tmp_file
+        array2+=($ws_domain)
     fi
 done
 
 sleep 30
 
 #第三次檢測 level3
-if [ -f "$tmp_file" ]; then
-    while read line; do
-        cnt=0
-        res=`python3 /root/gitlab-project/manager-tools/monitor/conn_ws.py $line 2>&1 | head -n 1 `
-        if [ "$res" != "ok" ];then
-            python $BOT "$GROUP" "From-${HOST}" "$(echo -e "Problem: WS Error {{fire}}{{fire}}{{fire}}Level3{{fire}}{{fire}}{{fire}} \nDomain: ${line}\nStatus: ${res}")"
-        fi
-    done  < $tmp_file
-fi
+for((i=0; i<${#array2[@]}; i++)); do
+    ws_domain=${array2[$i]}
+    #while read line; do
+    res=`python3 /root/gitlab-project/manager-tools/monitor/conn_ws.py $ws_domain 2>&1 | head -n 1 `
+    if [ "$res" != "ok" ];then
+        python $BOT "$GROUP" "From-${HOST}" "$(echo -e "Problem: WS Error {{fire}}{{fire}}{{fire}}Level3{{fire}}{{fire}}{{fire}} \nDomain: ${ws_domain}\nStatus: ${res}")"
+    fi
+done
 
-#刪除ws_domain暫存檔
-rm -rf $tmp_file
