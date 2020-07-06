1947d264db2acb08fc12839f7dadc4699024739b
James <james@bcowtech.com>
2020-07-06 07:28:54 +0000

    Update monitor_dns.sh

--- monitor/monitor_dns.sh
+++ monitor/monitor_dns.sh
@@ -2,34 +2,47 @@
 LIST='/root/gitlab-project/manager-tools/monitor/list.txt'
 BOT='/root/gitlab-project/manager-tools/python_bot/zbxtg_group.py'
 GROUP='BCowtech-alert'
+#GROUP='james-test'
 HOST=`hostname`
+array=()
+array2=()
 
+#第一次檢測 level1
 while read line; do
-
-    #8da3b5b2開頭是沒有開ping的域名
-    domain=`echo $line | grep -v '8da3b5b2' |cut -d "," -f1 `
-    echo $domain
-
-    #res=`ping -c 2 "$domain" 2>&1 | grep packets | cut -d "," -f 3 |  cut -d " " -f 2  | sed 's/%//g' `
-    res=`ping -c 2 "$domain" 2>&1`
-    
-    #檢查ping指令有沒有錯誤
-    value=`echo $?`
-
-    if [ "$value" = 0  ]; then
-        lost_value=`echo $res| grep packets | cut -d "," -f 3 |  cut -d " " -f 2  | sed 's/%//g' `
-        rtt_value=` echo $res| grep rtt | sed 's/^.*=//g'|cut -d "/" -f2`
-        #echo $rtt_value
-        #判斷 ping pack loss 的回傳值,如果>=50則輸出 pack loss 50%
-        if [ "$lost_value" -ge 50 ]; then
-            python $BOT "$GROUP" "Ping Error" "$domain Pack loss 50%"
-        fi
-
-        #判斷 ping rtt ms 的回傳值,如果>=100則輸出 pack loss 50%
-        if [ `echo "$rtt_value > 100" |bc` -eq 1 ];then
-            python $BOT "$GROUP" "From-${HOST} Ping Error" "$domain rtt average over 100ms"
-
-        fi
+    domain=`echo $line | cut -d "," -f1`
+    res=`nslookup $domain | grep -i "can't find"`
+    #判斷 nslookup 回傳值,如果有 grep 到 server can't find 字串,則解析失敗
+    if [  -n "$res"  ];then
+        #python $BOT "$GROUP" "From-${HOST} DNS Error" "$domain Parse error"
+        array+=($domain)
     fi
-
 done < $LIST
+
+#判斷arrya是否為空,有值才繼續執行
+if [ -n "$array" ];then
+    sleep 10
+
+    #第二次檢測 level2
+    for ((i=0; i<${#array[@]}; i++)); do
+        domain=${array[$i]}
+        res=`nslookup $domain | grep -i "can't find"`
+        if [  -n "$res"  ];then
+            python $BOT "$GROUP" "From-${HOST}" "$(echo -e "DNS Error {{fire}}{{fire}}Level2{{fire}}{{fire}}  \nDomain: $domain Parse error")"
+            array2+=($domain)
+        fi
+    done
+fi
+
+#判斷arrya2是否為空,有值才繼續執行
+if [ -n "$array2" ];then
+    sleep 30
+
+    #第三次檢測 level3
+    for ((i=0; i<${#array2[@]}; i++)); do
+        domain=${array2[$i]}
+        res=`nslookup $domain | grep -i "can't find"`
+        if [  -n "$res"  ];then
+            python $BOT "$GROUP" "From-${HOST}" "$(echo -e "DNS Error {{fire}}{{fire}}{{fire}}Level3{{fire}}{{fire}}{{fire}}  \nDomain: $domain Parse error")"
+        fi
+    done
+fi
